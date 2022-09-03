f88241b974245cd3df7aa5e4c60fee18e140b1ae
Edxon20 <andrade20eduardo@gmail.com>
2022-09-03 11:34:53 -0400

    Crear plantilla para agregar los gastos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,16 +2,36 @@ import React, {useState} from 'react';
 
 const Formulario = () => {
 
+    const [nombre, guardarNombre] = useState('');
+    const [cantidad,guardarCantidad] = useState(0); 
+
+    //Cuando el usuario agrega un gasto
+    const agregarGasto = e =>{
+        e.preventDefault();//Referente a las querys y se coloca para poder ejecutar nuestro codigo
+
+        // validar
+
+        // construir el gasto 
+
+        // Pasar el gasto al componente principal
+
+        //resetear el form 
+
+
+    }
+
     return(
 
         <form>
             <h2>Agrega tus gastos aqui</h2>
             <div className='campo'>
-                <label>Nombre del gasto </label>
+                <label>Nombre del gasto  </label>
                 <input 
                     type='text'
                     className='u-full-width'
                     placeholder='Ejemplo transporte'
+                    value={nombre}                                  //Uso en conjunto # nombre viene del hook
+                    onChange={e => guardarNombre(e.target.value)}   //Uso en conjunto ##
                 />
             </div>
 
@@ -21,6 +41,8 @@ const Formulario = () => {
                     type='number'
                     className='u-full-width'
                     placeholder='Ejemplo 300'
+                    value={cantidad}
+                    onChange={e => guardarCantidad(parseInt(e.target.value))} 
                 />
             </div>
 
