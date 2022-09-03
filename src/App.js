f00b30acb543e1579d166c605f814cabffe5848a
Edxon20 <andrade20eduardo@gmail.com>
2022-09-03 11:24:41 -0400

    Carga condicional de componente princial

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //definir el state
   const [presupuesto,guardarPresupuesto] = useState(0);
   const [restante,guardarRestante] = useState(0);
+  const [mostrarpregunta,actualizarPregunta ] = useState(true);
 
 
   return (
@@ -17,20 +18,24 @@ function App() {
 
 
         <div className='contenido-principal contenido'>
+          { mostrarpregunta ? 
+          (
           <Pregunta
             guardarPresupuesto={guardarPresupuesto}
             guardarRestante={guardarRestante}
+            actualizarPregunta={actualizarPregunta}
           />
+          ) :  (
+              <div className='row'>
+                <div className='one-half column'>
+                  <Formulario />
+                </div>
 
-          <div className='row'>
-            <div className='one-half column'>
-              <Formulario/>
-            </div>
-
-            <div className='one-half column'>
-              2
-            </div>
-          </div>
+                <div className='one-half column'>
+                  2
+                </div>
+              </div>
+          ) }              
         </div>
       </header>
     </div>
